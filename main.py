38bbdcbb9b4c9ff1fc49b8e30c612eae4f9b16eb
万事可乐 <silentmiss@outlook.com>
2023-10-10 22:51:06 +0800

    Update main.py
    add clash headers

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import requests
 
 suburl="https://sub.xn--lmq622c7qjhl4a.com/api/v1/client/subscribe?token=e4b320e9f72ee607a3c2af21cc3e4423"
+headers = {
+    "User-Agent": "ClashforWindows/0.20.36"
+}
 
-subtext = requests.get(suburl).text
-f=open('sub1','w')
+subtext = requests.get(url=suburl,headers=headers).text
+f=open('亿点连接(github)','w')
 f.write(subtext)
 f.close()
